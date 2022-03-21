aa5d44d85310e670a1bc822701df5907d14a7bd2
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-21 09:16:33 +0600

    export in react

---
+++ note/note.js
@@ -0,0 +1,17 @@
+//47_5-1 Module Introduction ES6 Modules simple import, export
+//============================================================
+/* 
+Module: dividing the file and get the file if needed.
+
+if there is no "./" in import, it means the import is from node module.
+
+no need to write extension while importing js file into another file.
+
+47_5-2 ES6 Modules, import default, relative path
+
+module can be any js data not only the component.
+
+one function can be export as default.
+more than one function export as object like: export {add, multiply};
+
+*/
