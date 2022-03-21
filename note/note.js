273ee55efb7da22130225fce9c563bc8d5683e48
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-21 10:58:34 +0600

    adding some info

--- note/note.js
+++ note/note.js
@@ -7,11 +7,30 @@ if there is no "./" in import, it means the import is from node module.
 
 no need to write extension while importing js file into another file.
 
-47_5-2 ES6 Modules, import default, relative path
-
+//47_5-2 ES6 Modules, import default, relative path
+==============================================================
 module can be any js data not only the component.
 
 one function can be export as default.
+
 more than one function export as object like: export {add, multiply};
+
+practice*** module function and export and import.
+
+//47_5-3 Create and Load Fake data, json generator, online api
+==============================================================
+
+some ways to loading data.
+
+1. getting data from same module.
+2. generating data for json generator then placing a file to public folder.
+3. mock api (fake data)
+
+
+//47_5-4 How to Create Event handler with parameters
+=============================================================
+
+
+
 
 */
